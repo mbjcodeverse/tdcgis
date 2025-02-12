5b7ca8756f3bce29d01609670e4f41149d66e0dd
mbjcodeverse <markhjolive@gmail.com>
2025-02-13 03:07:15 +0800

    change database host to local

--- models/connection.php
+++ models/connection.php
@@ -2,9 +2,9 @@
 
 class Connection{
 	public function connect(){
-		// $link = new PDO("mysql:host=localhost;dbname=gis", "root", "");
+		$link = new PDO("mysql:host=localhost;dbname=gis", "root", "");
 
-		$link = new PDO("mysql:host=localhost;dbname=u342130502_gis", "u342130502_gis", "MOJ@2024_onwards");
+		// $link = new PDO("mysql:host=localhost;dbname=u342130502_gis", "u342130502_gis", "MOJ@2024_onwards");
 
 		$link -> exec("set names utf8");
 		return $link;
